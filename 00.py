6b910c66b7c79e559400f9ad24b7e57dc6e80fe6
Kai-Yip <tingxueye@gmail.com>
2014-07-01 20:36:46 +0800

    v7.1

--- 00.py
+++ 00.py
@@ -2,13 +2,7 @@
 import os
 import sys
 
-f1=open("/home/alber/KuaiPan/data_base/jd_content/app-mac/lda/mac.txt","r")
-txt=f1.readlines()
-f1.close()
-f2=open("/home/alber/KuaiPan/data_base/jd_content/app-mac/lda/mac-result.txt",'a')
-
-for line in txt:
-	a=" ".join(line.split())
-	line_extend=a+" "+a+" "+a+" "+a+" "+a+"\n"
-	f2.write(line_extend)
-f2.close()
+wordlist=['a',1,'b',2,'a',3,'c',3,'d',4]
+a=['d','e']
+a.extend(wordlist)
+print a
--- lineself_copy.py
+++ lineself_copy.py
@@ -2,10 +2,10 @@
 import os
 import sys
 
-f1=open("/home/alber/KuaiPan/data_base/jd_content/app-mac/lda/mac-clean.txt","r")
+f1=open("/home/alber/KuaiPan/data_base/jd_content/pacis.txt","r")
 txt=f1.readlines()
 f1.close()
-f2=open("/home/alber/KuaiPan/data_base/jd_content/app-mac/lda/mac-clean-result.txt",'a')
+f2=open("/home/alber/KuaiPan/data_base/jd_content/pacis1.txt",'a')
 
 for line in txt:
 	a=" ".join(line.split())
