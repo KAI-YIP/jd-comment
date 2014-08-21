5d3123109335aeda13c4a8676ee3a5516f2ffabd
KAI-YIP <tingxueye@gmail.com>
2014-08-21 14:15:29 +0800

    v0821

--- 00.py
+++ 00.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
-def avg(list_seg):
-	if len(list-seg) < 1:
-		return None
-	else:
-		return sum(self.sequence)/len(self.sequence)
 
-a=[1,2,3,4,5]
-print (avg(a))
+def abs(a,b):
+	if a>b:
+		return a-b
+	else:
+		return b-a
+
+x1=input("a=")
+x2=input("b=")
+print (abs(int(x1),int(x2)))
